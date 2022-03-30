6b68b7295b3cfb4317ddcec1e8c1bc125f5ca045
kekuopex <keksuxarevawex@gmail.com>
2022-03-30 17:22:57 +0300

    add htmlTable + array(php) + css class

---
+++ htmlTable.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-type: text/plain');
+$row_styles = array('even', 'odd');
+$style_index = 0;
+$meal = ['breakfast' => 'Walnut Bun',
+          'lunch' => 'Cashew Nuts and White Mushrooms',
+          'snack' => 'Dried Mulberries',
+          'dinner' => 'Eggplant with Chili Sauce'];
+print "<table>\n";
+foreach ($meal as $key => $value) {
+  print "<tr class = $row_styles[$style_index]>";
+  print "<td>$key</td><td>$value</td></tr>\n";
+  $style_index = 1 - $style_index;
+}
+print '</table>';
